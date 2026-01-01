2d664b3933e3fe8ac869cf118d86cae75584490b
snaku <harischali543@gmail.com>
2026-01-01 19:39:57 +0100

    forward declaration for 'BattleActorCore'

--- include/Battle/btlActor.h
+++ include/Battle/btlActor.h
@@ -4,41 +4,42 @@
 #include "Utils.h"
 
 typedef struct BattleActor BattleActor;
+typedef struct BattleActorCore BattleActorCore;
 struct BattleActorData;
 
 // TODO. This struct contains the pos, rot and scale (and other unknown things) of the actor
 // temporary name maybe ?
-typedef struct
+struct BattleActorCore 
 {
     // data before...
     u32 unkFlag_9c;                    // 0x9c
     // data inbetween...
     struct BattleActorData* actorData; // 0xa2c
-} BattleActorCore;
+};
 
 // TODO. 1200 bytes
 struct BattleActor
 {
     // data before...
-    u32 unk_08;                     // 0x8
+    u32 unk_08;                 // 0x8
     // data inbetween...
-    u16 currState;                  // 0xc
-    u16 pendingState;               // 0xe
-    u16 oldState;                   // 0x10
-    u16 pendingStateTimer;          // 0x12. If > 0, decrement every frame and if == 0, new state will be pendingState
-    u16 unk_14;                     // 0x14
-    u16 unk_18;                     // 0x18
-    u16 unk_1a;                     // 0x1a
-    u32 stateTimer;                 // 0x1c. 60hz timer (+=2 per frame at 30fps). Reset on state change
-    u32 unk_20;                     // 0x20
-    u32 idleWeaponAnimTimer;        // 0x24. Decrement every frame. If underflow, play animation
+    u16 currState;              // 0xc
+    u16 pendingState;           // 0xe
+    u16 oldState;               // 0x10
+    u16 pendingStateTimer;      // 0x12. If > 0, decrement every frame and if == 0, new state will be pendingState
+    u16 unk_14;                 // 0x14
+    u16 unk_18;                 // 0x18
+    u16 unk_1a;                 // 0x1a
+    u32 stateTimer;             // 0x1c. 60hz timer (+=2 per frame at 30fps). Reset on state change
+    u32 unk_20;                 // 0x20
+    u32 idleWeaponAnimTimer;    // 0x24. Decrement every frame. If underflow, play animation
     // data inbetween...
     BattleActorCore* actorCore; // 0x30
     // data inbetween...
-    u16 unk_36;                     // 0x36
+    u16 unk_36;                 // 0x36
     // data inbetween...
-    BattleActor* next;       // 0x4a4
-    BattleActor* prev;       // 0x4a8
+    BattleActor* next;          // 0x4a4
+    BattleActor* prev;          // 0x4a8
 }; 
 
 #endif
