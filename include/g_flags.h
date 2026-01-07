414271a312a981be7bc95723914be5550193de3e
snaku <harischali543@gmail.com>
2026-01-07 16:02:03 +0100

    Add global flag 'PENDING_DARK_HOUR'

--- include/g_flags.h
+++ include/g_flags.h
@@ -3,7 +3,8 @@
 
 // !! 'gGlobalFlags' array is in 'g_data.c' file !!
 
-#define G_FLAG_ROOM_TUTORIAL_SEEN   3086 // "If you go back into your room, it will become midnight". idx 96 (1 << 14)
+#define G_FLAG_PENDING_DARK_HOUR    2565 // idx 80 (1 << 5). When activated and the time is passing, trigger dark hour and tp hero to tartarus
+#define G_FLAG_ROOM_HELP_SEEN       3086 // idx 96 (1 << 14). "If you go back into your room, it will become midnight"
 #define G_FLAG_HERO_HANDS_IN_POCKET 3125 // idx 98 (1 << 21)
 
 #endif
