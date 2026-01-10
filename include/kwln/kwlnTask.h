c31b5faab710a21f099b214d49ef124468aed0a8
snaku <harischali543@gmail.com>
2026-01-10 02:22:50 +0100

    Create 'KwlnTask' struct

--- include/kwln/kwlnTask.h
+++ include/kwln/kwlnTask.h
@@ -1,6 +1,19 @@
 #ifndef KWLNTASK_H
 #define KWLNTASK_H
 
-// TODO
+#include "Utils.h"
+
+// 104 bytes (not sure)
+typedef struct KwlnTask
+{
+    u8 taskName[24];
+    u8 unkData1[0x08];
+    u32 unk_20;
+    u8 unkData2[0x14];
+    s32 (*Update)(struct KwlnTask* task);   // maybe return 0 = continue task and return -1 = destroy task
+    void (*Destroy)(struct KwlnTask* task);
+    void* taskData;                         // Pointer to data specific to the curr task (like a Camera struct)
+    u8 unkData3[0x24];
+} KwlnTask;
 
 #endif
