461d6b29fc4a134e104511efe918dc8ff6bdb050
snaku <harischali543@gmail.com>
2026-01-10 06:24:32 +0100

    add 'rootProcTask' in 'GlobalCtx' struct

--- include/temporary.h
+++ include/temporary.h
@@ -34,8 +34,9 @@ typedef struct
     u8 unkData1[0x124];
     u32 scenarioMode;       // 007cdfa4. See enum GameScenario
     u8 unkData2[0x448];
-    BattleCtx* btlCtx;      // 007ce3ec. NULL when not in a battle
+    KwlnTask* rootProcTask; // 007ce07c
     KwlnTask* dungeonTask;  // 007ce268. NULL when not in tartarus. Task name = "automatic dungeon"
+    BattleCtx* btlCtx;      // 007ce3ec. NULL when not in a battle
 } GlobalCtx;
 
 extern GlobalCtx ctx; // 007cde80
