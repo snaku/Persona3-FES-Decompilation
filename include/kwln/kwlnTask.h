f2a3736557f14a50179471901ae2f2b4fc464b57
snaku <harischali543@gmail.com>
2026-01-10 06:49:44 +0100

    Starting to decomp 'KwlnTask_Init' + add 'taskTimer' to 'KwlnTask' struct

--- include/kwln/kwlnTask.h
+++ include/kwln/kwlnTask.h
@@ -14,11 +14,14 @@ typedef struct KwlnTask
     u8 taskName[24];
     u8 unkData1[0x08];
     u32 unk_20;
-    u8 unkData2[0x14];
+    u32 taskTimer;
+    u8 unkData2[0x10];
     KwlnTask_Update update;   // maybe return 0 = continue task and return -1 = destroy task
     KwlnTask_Destroy destroy;
     void* taskData;           // Pointer to data specific to the curr task (like a Camera struct)
     u8 unkData3[0x24];
 } KwlnTask;
+
+KwlnTask* KwlnTask_Init(u8* taskName, u32 param_2, KwlnTask_Update update, KwlnTask_Destroy destroy, void* taskData);
 
 #endif
--- src/kwln/kwlnTask.c
+++ src/kwln/kwlnTask.c
@@ -1,3 +1,31 @@
 #include "kwln/kwlnTask.h"
+#include "g_data.h"
 
-// TODO
+// FUN_00194c50
+KwlnTask* KwlnTask_Init(u8* taskName, u32 param_2, KwlnTask_Update update, KwlnTask_Destroy destroy, void* taskData)
+{
+    // TODO !!
+
+    KwlnTask* task;
+
+    if (taskName[0] == '\0')
+    {
+        P3FES_ASSERT("kwlnTask.c", 1022);
+    }
+
+    // !! ALLOC !!
+    // task = FUN_00191af0(sizeof(KwlnTask)); 
+
+    if (task == NULL)
+    {
+        P3FES_ASSERT("kwlnTask.c", 1032);
+        return NULL;
+    }
+
+    task->unk_20 = param_2;
+    task->unk_24 = 0;
+    task->taskTimer = 0;
+    task->update = update;
+    task->destroy = destroy;
+    task->taskData = taskData;
+}
