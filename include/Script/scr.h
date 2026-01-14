a0ffd4e1cd50a1433b459379eda0cdda4c99442b
snaku <harischali543@gmail.com>
2026-01-14 22:29:38 +0100

    Create 'ScrValues' union and add 'argValues' in 'ScriptData' struct

--- include/Script/scr.h
+++ include/Script/scr.h
@@ -5,23 +5,30 @@
 
 #define SCR_MAX_ARGS 26
 
+typedef struct KwlnTask KwlnTask;
+
 typedef enum
 {
     SCR_TYPE_INT,
     SCR_TYPE_FLOAT
 } ScrTypes;
 
-typedef struct KwlnTask KwlnTask;
+typedef union 
+{
+    s32 iVal;
+    f32 fVal;
+} ScrValues;
 
 // 252 bytes
 typedef struct ScriptData
 {
-    u8 scrName[24];            // same as scrTask->taskName
+    u8 scrName[24];                    // same as scrTask->taskName
     u32 unk_18;
-    u32 argCount;              // 0x1c
-    u8 argTypes[SCR_MAX_ARGS]; // 0x20. See enum 'ScrTypes'
-    u8 unkData1[0xaa];
-    KwlnTask* scrTask;         // 0xe4
+    u32 argCount;                      // 0x1c
+    u8 argTypes[SCR_MAX_ARGS];         // 0x20. See enum 'ScrTypes'
+    ScrValues argValues[SCR_MAX_ARGS]; // 0x3c
+    u8 unkData1[0x7c];
+    KwlnTask* scrTask;                 // 0xe4
     u8 unkData2[0x14];
 } ScriptData;
 
