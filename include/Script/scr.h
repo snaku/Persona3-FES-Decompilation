ee1d14fb632d8553bdf80752def005abf60c586f
snaku <harischali543@gmail.com>
2026-01-14 22:46:10 +0100

    Rename struct 'ScriptData' to 'ProcedureData'

--- include/Script/scr.h
+++ include/Script/scr.h
@@ -19,17 +19,17 @@ typedef union
     f32 fVal;
 } ScrValues;
 
-// 252 bytes
-typedef struct ScriptData
+// 252 bytes. Data of a function
+typedef struct ProcedureData
 {
-    u8 scrName[24];                    // same as scrTask->taskName
+    u8 prcdName[24];                   // same as prcdTask->taskName
     u32 unk_18;
     u32 argCount;                      // 0x1c
     u8 argTypes[SCR_MAX_ARGS];         // 0x20. See enum 'ScrTypes'
     ScrValues argValues[SCR_MAX_ARGS]; // 0x3c
     u8 unkData1[0x7c];
-    KwlnTask* scrTask;                 // 0xe4
+    KwlnTask* prcdTask;                // 0xe4
     u8 unkData2[0x14];
-} ScriptData;
+} ProcedureData;
 
 #endif
