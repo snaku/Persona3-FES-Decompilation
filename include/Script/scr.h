4b82ded6aa6b31eccc49842a4c00f114f92b49ad
snaku <harischali543@gmail.com>
2026-01-14 22:13:43 +0100

    Create enum 'ScrTypes'

--- include/Script/scr.h
+++ include/Script/scr.h
@@ -5,6 +5,12 @@
 
 #define SCR_MAX_ARGS 26
 
+typedef enum
+{
+    SCR_TYPE_INT,
+    SCR_TYPE_FLOAT
+} ScrTypes;
+
 typedef struct KwlnTask KwlnTask;
 
 // 252 bytes
@@ -13,7 +19,7 @@ typedef struct ScriptData
     u8 scrName[24];            // same as scrTask->taskName
     u32 unk_18;
     u32 argCount;              // 0x1c
-    u8 argTypes[SCR_MAX_ARGS]; // 0x20
+    u8 argTypes[SCR_MAX_ARGS]; // 0x20. See enum 'ScrTypes'
     u8 unkData1[0xaa];
     KwlnTask* scrTask;         // 0xe4
     u8 unkData2[0x14];
