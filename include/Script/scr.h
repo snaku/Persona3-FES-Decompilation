93f13a27b99f829dfb2fdaead6a05700fbd3d9c9
snaku <harischali543@gmail.com>
2026-01-14 05:47:18 +0100

    Starting 'ScriptData' struct

--- include/Script/scr.h
+++ include/Script/scr.h
@@ -1,6 +1,17 @@
 #ifndef SCR_H
 #define SCR_H
 
-// TODO
+#include "Utils.h"
+
+typedef struct KwlnTask KwlnTask;
+
+// 252 bytes
+typedef struct ScriptData
+{
+    u8 scrName[24];    // same as scrTask->taskName
+    u8 unkData1[0xcc];
+    KwlnTask* scrTask; // 0xe4
+    u8 unkData2[0x14];
+} ScriptData;
 
 #endif
