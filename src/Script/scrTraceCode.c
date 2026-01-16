4698efbd963522fe4ac2da674ebfaea222a2f052
snaku <harischali543@gmail.com>
2026-01-17 00:31:08 +0100

    Fix 'Scr_ExecOpCode' having a 'ScrHeader' ptr arg instead of 'ScrData' ptr

--- src/Script/scrTraceCode.c
+++ src/Script/scrTraceCode.c
@@ -1,10 +1,11 @@
 #include "Script/scr.h"
 #include "Script/scrTraceCode.h"
 
-typedef u32 (*Scr_ExecOpCode)(ScrHeader* header);
+typedef u32 (*Scr_ExecOpCode)(ScrData* header);
 
 // 0069d3e0
-static const Scr_ExecOpCode OpCodeFuncTable[] =
+static const Scr_ExecOpCode opCodeFuncTable[] =
 {
     // TODO
 };
+
