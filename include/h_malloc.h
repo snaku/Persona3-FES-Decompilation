efa5e78c94efeeb587fa0c36f78f0461419e9056
snaku <harischali543@gmail.com>
2026-01-19 15:38:47 +0100

    Create 'h_malloc' files and starting to decomp 'H_Malloc' func

---
+++ include/h_malloc.h
@@ -0,0 +1,8 @@
+#ifndef H_MALLOC_H
+#define H_MALLOC_H
+
+#include "Utils.h"
+
+uintptr_t H_Malloc(u32 size);
+
+#endif
---
+++ src/h_malloc.c
@@ -0,0 +1,11 @@
+#include "h_malloc.h"
+
+// FUN_00191af0
+uintptr_t H_Malloc(u32 size)
+{
+    uintptr_t addr;
+
+    // TODO
+
+    return addr;
+}
