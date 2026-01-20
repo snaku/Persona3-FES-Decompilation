c667086241e764f51c5d0392e6f5cf22014793a4
snaku <harischali543@gmail.com>
2026-01-20 11:39:10 +0100

    add 'unkTask1', 'unkTask2' and 'unkTask3' to 'GlobalCtx' struct

--- include/temporary.h
+++ include/temporary.h
@@ -34,14 +34,16 @@ typedef struct
     u8 unkData1[0x124];
     u32 scenarioMode;       // 007cdfa4. See enum GameScenario
     u8 unkData2[0x448];
-    KwlnTask* rootProcTask; // 007ce07c
+    KwlnTask* unkTask1;     // 007ce064
+    KwlnTask* unkTask2;     // 007ce070
+    KwlnTask* unkTask3;     // 007ce074
+    KwlnTask* rootProcTask; // 007ce07c. Task name = "root proc"
     KwlnTask* dungeonTask;  // 007ce268. NULL when not in tartarus. Task name = "automatic dungeon"
     BattleCtx* btlCtx;      // 007ce3ec. NULL when not in a battle
 } GlobalCtx;
 
 extern GlobalCtx ctx; // 007cde80
 
-u32 GlobalCtx_GetCurrentTartarusFloor();
 void* P3FES_Memset(void* dst, u8 value, u32 size);
 
 #endif
