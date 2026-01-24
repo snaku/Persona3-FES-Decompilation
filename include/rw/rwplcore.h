75ef3375db8d774fcafad24b5a32276d670944e7
snaku <harischali543@gmail.com>
2026-01-24 10:57:20 +0100

    Create most of renderware types

--- include/rw/rwplcore.h
+++ include/rw/rwplcore.h
@@ -1,6 +1,23 @@
 #ifndef RWPLCORE_H
 #define RWPLCORE_H
 
-// TODO
+// RENDERWARE TYPES
+typedef int RwFixed;
+typedef int RwInt32;
+typedef unsigned int RwUInt32;
+
+typedef short RwInt16;
+typedef unsigned short RwUInt16;
+
+typedef unsigned char RwUInt8;
+typedef signed char RwInt8;
+typedef char RwChar;
+
+typedef long RwInt64;
+typedef unsigned long RwUInt64;
+
+typedef float RwReal;
+
+typedef RwInt32 RwBool;
 
 #endif
