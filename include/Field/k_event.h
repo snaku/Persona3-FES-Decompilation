6d21a46431c4b3384c7fa989b3f1cd6a3f42b627
snaku <harischali543@gmail.com>
2026-01-25 17:04:11 +0100

    Create 'FieldEvent' struct

--- include/Field/k_event.h
+++ include/Field/k_event.h
@@ -3,6 +3,13 @@
 
 #include "Utils.h"
 
-
+// 288 bytes
+typedef struct FieldEvent
+{
+    u32 eventType;         // 0x00
+    u8 unkData1[0x58];
+    KwlnTask* drawCmdTask; // 0x5c
+    u8 unkData2[0xc0];
+} FieldEvent;
 
 #endif
