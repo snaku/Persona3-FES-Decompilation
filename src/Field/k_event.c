ffbde8bff214001bb26bd5646ec8fe0aa8d1edb3
snaku <harischali543@gmail.com>
2026-01-25 17:24:24 +0100

    Implement 'FldEvent_CreateDrawCmdTask' and 'FldEvent_DestroyDrawCmdTask'

--- src/Field/k_event.c
+++ src/Field/k_event.c
@@ -2,3 +2,32 @@
 #include "Field/k_event.h"
 #include "rw/rwplcore.h"
 
+KwlnTask* FldEvent_CreateDrawCmdTask(KwlnTask* fldEventTask);
+
+// FUN_001cd690
+s32 FldEvent_UpdateDrawCmdTask(KwlnTask* drawCmdTask)
+{
+    // TODO
+
+    return KWLN_TASK_CONTINUE;
+}
+
+// FUN_001cd6e0
+void FldEvent_DestroyDrawCmdTask(KwlnTask* drawCmdTask)
+{
+    rwGlobals.memFuncs.Rw_Free(drawCmdTask->taskData);
+}
+
+// FUN_001cd710
+KwlnTask* FldEvent_CreateDrawCmdTask(KwlnTask* fldEventTask)
+{
+    FieldDrawCmd* drawCmd;
+
+    drawCmd = rwGlobals.memFuncs.Rw_Calloc(1, sizeof(FieldDrawCmd), 0x40000);
+    if (drawCmd == NULL)
+    {
+        return NULL;
+    }
+
+    return KwlnTask_CreateWithAutoPriority(fldEventTask, 4207, "draw command", FldEvent_UpdateDrawCmdTask, FldEvent_DestroyDrawCmdTask, drawCmd);
+}
