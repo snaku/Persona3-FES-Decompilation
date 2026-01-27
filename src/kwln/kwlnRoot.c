7da9f7cb0098812c3a62f6ca9ca62a9f279c46f7
snaku <harischali543@gmail.com>
2026-01-27 17:35:32 +0100

    '2D Draw End' task DONE

--- src/kwln/kwlnRoot.c
+++ src/kwln/kwlnRoot.c
@@ -1,11 +1,12 @@
 #include "kwln/kwlnRoot.h"
 #include "kwln/kwlnTask.h"
-#include "rw/rwplcore.h"
+#include "rw/rwcore.h"
 #include "temporary.h"
 #include "h_snd.h"
 
 KwlnTask* KwlnRoot_Create2DDrawBeginTask();
 KwlnTask* KwlnRoot_Create2DDrawBeginPreEndTask();
+KwlnTask* KwlnRoot_Create2DDrawEndTask();
 
 // FUN_00198590. Not sure if it's in this file or in 'kwln.c' ?
 RwCamera* KwlnRoot_GetMainCamera()
@@ -62,6 +63,15 @@ s32 KwlnRoot_Update2DDrawPreEndTask(KwlnTask* drawBegin2dPETask)
     return KWLN_TASK_CONTINUE;
 }
 
+// FUN_00198b20
+s32 KwlnRoot_Update2DDrawEndTask(KwlnTask* drawEnd2dTask)
+{
+    RwCamera* camera = KwlnRoot_GetMainCamera();
+    RwCamera_EndUpdate(camera);
+
+    return KWLN_TASK_CONTINUE;
+}
+
 // FUN_00198b50
 KwlnTask* KwlnRoot_Create2DDrawBeginTask()
 {
@@ -72,4 +82,10 @@ KwlnTask* KwlnRoot_Create2DDrawBeginTask()
 KwlnTask* KwlnRoot_Create2DDrawBeginPreEndTask()
 {
     return KwlnTask_Init("2D Draw Begin Pre End", 5231, KwlnRoot_Update2DDrawPreEndTask, NULL, NULL);
+}
+
+// FUN_00198bd0
+KwlnTask* KwlnRoot_Create2DDrawEndTask()
+{
+    return KwlnTask_Init("2D Draw End", 5241, KwlnRoot_Update2DDrawEndTask, NULL, NULL);
 }
