0dd6e1041f59a119900ddb0b459997e6e25ea650
snaku <harischali543@gmail.com>
2026-01-27 14:58:02 +0100

    Create 'RwCamera' struct

--- include/rw/rwcore.h
+++ include/rw/rwcore.h
@@ -1,6 +1,19 @@
 #ifndef RWCORE_H
 #define RWCORE_H
 
-#include "Utils.h"
+#include "rw/rwplcore.h"
+
+struct RwCamera;
+
+typedef struct RwCamera* (*RwCameraBeginUpdateFunc)(struct RwCamera* camera);
+typedef struct RwCamera* (*RwCameraEndUpdateFunc)(struct RwCamera* camera);
+
+// TODO
+typedef struct RwCamera
+{
+    u8 unkData1[0x18];
+    RwCameraBeginUpdateFunc beginUpdate; // 0x18
+    RwCameraEndUpdateFunc endUpdate;     // 0x1c
+} RwCamera;
 
 #endif
