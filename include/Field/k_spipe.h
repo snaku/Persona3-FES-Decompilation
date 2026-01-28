d68a7f06e255a82297a28cafeaae01b82425c524
snaku <harischali543@gmail.com>
2026-01-28 18:13:38 +0100

    Create 'k_spipe' files

---
+++ include/Field/k_spipe.h
@@ -0,0 +1,8 @@
+#ifndef K_SPIPE_H
+#define K_SPIPE_H
+
+#include "Utils.h"
+
+typedef struct KwlnTask KwlnTask;
+
+#endif
---
+++ src/Field/k_spipe.c
@@ -0,0 +1,4 @@
+#include "kwln/kwlnTask.h"
+#include "Field/k_spipe.h"
+#include "temporary.h"
+
