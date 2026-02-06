5be50d04fc053c20267c00b4be45f811997b4198
snaku <harischali543@gmail.com>
2026-02-06 18:03:24 +0100

    Add 'eplKosakaTask' in 'DungeonData' struct

--- include/Field/k_dungeon.h
+++ include/Field/k_dungeon.h
@@ -3,17 +3,22 @@
 
 #include "Utils.h"
 
+typedef struct KwlnTask KwlnTask;
+
 typedef enum
 {
     // TODO
     DUNGEON_STATE_STOP = 9
 } DungeonState;
 
-// TODO
+// 64 bytes. TODO
 typedef struct DungeonData
 {
-    u16 state;
-    u32 currFloor;
+    u16 state;               // 0x00
+    u32 currFloor;           // 0x04
+    u8 unkData1[0x14];
+    KwlnTask* eplKosakaTask; // 0x1c
+    u8 unkData2[0x20];
 } DungeonData;
 
 u32 Dungeon_GetCurrentFloor();
