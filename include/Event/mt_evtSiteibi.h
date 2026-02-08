1c38bb568dd6c3776321d021868ce0cc0b998772
snaku <harischali543@gmail.com>
2026-02-08 20:40:59 +0100

    Create 'mt_evtSiteibi.h' + 'EventSiteibiIdx' enum and 'SiteibiEvent' struct

---
+++ include/Event/mt_evtSiteibi.h
@@ -0,0 +1,28 @@
+#ifndef MT_EVTSITEIBIIDX_H
+#define MT_EVTSITEIBIIDX_H
+
+#include "Utils.h"
+
+// 'Siteibi' is maybe a typo.
+// It should be 'Shiteibi' which means 'fixed date' in japanese 
+// so SiteibiEvent = FixedDateEvent
+
+// To use for a 'SiteibiEvent' array
+typedef enum
+{
+    EVENT_SITEIBI_GAME_MODE,
+    EVENT_SITEIBI_INTRO_ANIME_CS, 
+    // TODO
+} EventSiteibiIdx;
+
+// 24 bytes
+typedef struct SiteibiEvent
+{
+    u8 month;          // 0x00
+    u8 day;            // 0x01
+    u8 time;           // 0x02
+    u16 scrPrcdIdx;    // 0x04. Procedure index of the event in 'SiteibiEvent.BF' file
+    u8 unkData2[0x12];
+} SiteibiEvent;
+
+#endif
