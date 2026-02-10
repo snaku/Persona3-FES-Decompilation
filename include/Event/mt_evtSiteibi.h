e5cd36e85a494570531dc42a2d52b1a975f538ab
snaku <harischali543@gmail.com>
2026-02-10 06:10:05 +0100

    Add 'gflagToActivate' in 'SiteibiEvent' struct

--- include/Event/mt_evtSiteibi.h
+++ include/Event/mt_evtSiteibi.h
@@ -18,11 +18,12 @@ typedef enum
 // 24 bytes
 typedef struct SiteibiEvent
 {
-    u8 month;          // 0x00
-    u8 day;            // 0x01
-    u8 time;           // 0x02
-    u16 scrPrcdIdx;    // 0x04. Procedure index of the event in 'SiteibiEvent.BF' file
-    u8 unkData2[0x12];
+    u8 month;            // 0x00
+    u8 day;              // 0x01
+    u8 time;             // 0x02
+    u16 scrPrcdIdx;      // 0x04. Procedure index of the event in 'SiteibiEvent.BF' file
+    u8 unkData2[0x10];
+    u16 gflagToActivate; // 0x16. 'G_FLAG_*' that the event needs to activate
 } SiteibiEvent;
 
 #endif
