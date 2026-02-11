c960b3229261b7708672d872881dc3c9f6a40968
snaku <harischali543@gmail.com>
2026-02-12 00:03:45 +0100

    Create 'ScrCommonCommand' struct

--- include/Script/scrCommonCommand.h
+++ include/Script/scrCommonCommand.h
@@ -1,6 +1,13 @@
 #ifndef SCRCOMMONCOMMAND_H
 #define SCRCOMMONCOMMAND_H
 
-// TODO
+#include "Utils.h"
+
+// 8 bytes
+typedef struct
+{
+    u8 (*Scr_Cmd)(); // 0x00
+    u32 totalParam;  // 0x04
+} ScrCommonCommand;
 
 #endif
