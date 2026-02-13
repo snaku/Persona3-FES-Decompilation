6f55c60750eb96c948fe49318cea61485744a90c
snaku <harischali543@gmail.com>
2026-02-13 19:29:50 +0100

    Implement 'BtlMain_SetBossBtlFlag'

--- include/Battle/btlMain.h
+++ include/Battle/btlMain.h
@@ -1,6 +1,8 @@
 #ifndef BTLMAIN_H
 #define BTLMAIN_H
 
+#include "Utils.h"
 
+void BtlMain_SetBossBtlFlag();
 
 #endif
--- src/Battle/btlMain.c
+++ src/Battle/btlMain.c
@@ -1,2 +1,9 @@
 #include "Battle/btlMain.h"
+#include "Battle/btlCtx.h"
+#include "temporary.h"
 
+// FUN_0027d8b0
+void BtlMain_SetBossBtlFlag()
+{
+    ctx.btlCtx->flags |= BATTLE_FLAG_IS_BOSS_BTL;
+}
