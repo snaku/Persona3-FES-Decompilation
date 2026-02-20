96e8fd0fa1d172e6f882e9d483a22c10fc46e4e9
snaku <harischali543@gmail.com>
2026-02-20 09:31:34 +0100

    Fix 'BattleActor' struct to have correct offsets and size

--- include/Battle/btlActor.h
+++ include/Battle/btlActor.h
@@ -20,26 +20,27 @@ struct BattleActorCore
 // TODO. 1200 bytes
 struct BattleActor
 {
-    // data before...
+    u8 unkData1[0x08];
     u32 actorId;                // 0x8
-    // data inbetween...
     u16 currState;              // 0xc. See enum 'BattleActionState'
     u16 pendingState;           // 0xe. See enum 'BattleActionState'
     u16 oldState;               // 0x10
     u16 pendingStateTimer;      // 0x12. If > 0, decrement every frame and if == 0, new state will be pendingState
     u16 unk_14;                 // 0x14
+    s16 unk_16;                 // 0x16
     u16 unk_18;                 // 0x18
     u16 unk_1a;                 // 0x1a
     u32 stateTimer;             // 0x1c. 60hz timer (+=2 per frame at 30fps). Reset on state change
     u32 unk_20;                 // 0x20
     u32 idleWeaponAnimTimer;    // 0x24. Decrement every frame. If underflow, play animation
-    // data inbetween...
+    u8 unkData1[0x08];
     BattleActorCore* actorCore; // 0x30
-    // data inbetween...
+    s16 unk_34;                 // 0x34
     u16 unk_36;                 // 0x36
-    // data inbetween...
+    u8 unkData1[0x46c];
     BattleActor* next;          // 0x4a4
     BattleActor* prev;          // 0x4a8
-}; 
+    s32 unk_4ac;                // 0x4ac
+};
 
 #endif
