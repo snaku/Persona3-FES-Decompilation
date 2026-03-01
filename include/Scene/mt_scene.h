cd7ac6c7d8d09f3b3868615258be2cdd44090acc
snaku <harischali543@gmail.com>
2026-03-01 12:37:40 +0100

    Create 'mt_scene' files and 'Mt_Scene' struct

---
+++ include/Scene/mt_scene.h
@@ -0,0 +1,16 @@
+#ifndef MT_SCENE_H
+#define MT_SCENE_H
+
+#include "Utils.h"
+
+// 40 bytes ? Not sure
+typedef struct
+{
+    s32 fldMajorId;   // 0x00
+    s32 fldMinorId;   // 0x04
+    u8 unkData[0x20];
+} Mt_Scene;
+
+extern Mt_Scene* gMtScene;
+
+#endif
---
+++ src/Scene/mt_scene.c
@@ -0,0 +1,4 @@
+#include "Scene/mt_scene.h"
+
+static Mt_Scene sMtScene;     // 0095afc0
+Mt_Scene* gMtScene = &sMtScene; // 007cd540
