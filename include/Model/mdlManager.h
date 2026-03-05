892b082281b88c8039092f3e840821d95b5ac7eb
snaku <harischali543@gmail.com>
2026-03-05 12:54:26 +0100

    Create 'ModelType' enum

--- include/Model/mdlManager.h
+++ include/Model/mdlManager.h
@@ -4,16 +4,31 @@
 #include "Utils.h"
 #include "rw/rwplcore.h"
 
+typedef enum
+{
+    MODEL_TYPE_BTLCHAR = 1, // bc*.RMD    - "MODEL/PACK/" (in .pac)
+    MODEL_TYPE_ENEMY,       // EM*.RMD    - "MODEL/PACK/" (in .pac)
+    MODEL_TYPE_PERSONA,     // PS*.RMD    - "MODEL/PERSONA/"
+    MODEL_TYPE_NPC = 5,     // N*.RMD     - "MODEL/NPC/"
+    MODEL_TYPE_WEAPON = 7,  // WP*.RMD    - "MODEL/WEAPON/"
+    MODEL_TYPE_ENEMYSYMBOL, // ES*.RMD    - "MODEL/SYMBOL/"
+    MODEL_TYPE_FLDCHAR,     // FC_*_*.RMD - "MODEL/FIELD/"
+    MODEL_TYPE_WEAPONH,     // WP*_H.RMD  - "MODEL/WEAPON/"
+    MODEL_TYPE_PERSONAFCL   // FP*.RMD    - "MODEL/FACILITYP/"
+} ModelType;
+
 // 1072 bytes
 typedef struct Model
 {
     RwMatrix mat;      // 0x00
     u8 unkData1[0x94];
-    u16 type;          // 0xd4
+    u16 type;          // 0xd4. See enum 'ModelType'
     u16 id;            // 0xd6
     u8 unkData[0x358];
 } Model;
 
+Model* MdlManager_InitMdl(u32 type, u32 id);
+Model* MdlManager_CreateMdl(u32 type, u32 id, u32 flags);
 RwMatrix* MdlManager_GetMdlMatrix(Model* mdl);
 
 #endif
--- src/Model/mdlManager.c
+++ src/Model/mdlManager.c
@@ -1,5 +1,23 @@
 #include "Model/mdlManager.h"
 
+#define MODEL_PATH "model/"
+
+// FUN_00316690. TODO
+Model* MdlManager_InitMdl(u32 type, u32 id)
+{
+    // TODO
+
+    return NULL;
+}
+
+// FUN_00316e00. TODO
+Model* MdlManager_CreateMdl(u32 type, u32 id, u32 flags)
+{
+    // TODO
+
+    return NULL;
+}
+
 // FUN_00318b60
 RwMatrix* MdlManager_GetMdlMatrix(Model* mdl)
 {
