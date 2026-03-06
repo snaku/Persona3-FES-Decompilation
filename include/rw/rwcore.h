44cdd852d2a2ccda5b3ffcefbfefd7f72bad01f5
snaku <harischali543@gmail.com>
2026-03-07 00:21:45 +0100

    Create 'RwRaster' struct

--- include/rw/rwcore.h
+++ include/rw/rwcore.h
@@ -3,6 +3,30 @@
 
 #include "rw/rwplcore.h"
 
+typedef struct RwRaster RwRaster;
+
+// 52 bytes
+struct RwRaster
+{
+    RwRaster* parent;        // 0x00
+    RwUInt8* cpPixels;       // 0x04
+    RwUInt8* palette;        // 0x08
+    RwInt32 width;           // 0x0c
+    RwInt32 height;          // 0x10
+    RwInt32 depth;           // 0x14
+    RwInt32 stride;          // 0x18
+    RwInt16 nOffsetX;        // 0x1c
+    RwInt16 nOffsetY;        // 0x1e
+    RwUInt8 cType;           // 0x20
+    RwUInt8 cFlags;          // 0x21
+    RwUInt8 privateFlags;    // 0x22
+    RwUInt8 cFormat;         // 0x23
+    RwUInt8* originalPixels; // 0x24
+    RwInt32 originalWith;    // 0x28
+    RwInt32 originalHeight;  // 0x2c
+    RwInt32 originalStride;  // 0x30
+};
+
 struct RwCamera;
 
 typedef struct RwCamera* (*RwCameraBeginUpdateFunc)(struct RwCamera* camera);
@@ -18,7 +42,7 @@ typedef enum
 typedef struct RwCamera
 {
     u8 unkData1[0x14];
-    RwCameraProjectionType projType;      // 0x14
+    RwCameraProjectionType projType;     // 0x14
     RwCameraBeginUpdateFunc beginUpdate; // 0x18
     RwCameraEndUpdateFunc endUpdate;     // 0x1c
 } RwCamera;
