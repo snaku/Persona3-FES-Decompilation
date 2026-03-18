fee41e9c232e005a905029f5ee639b44ef143b65
snaku <harischali543@gmail.com>
2026-03-18 15:28:19 +0100

    Starting 'H_Cdvd' struct

--- include/h_cdvd.h
+++ include/h_cdvd.h
@@ -5,6 +5,18 @@
 
 #define MAX_PATH_SIZE 255
 
+typedef struct H_Cdvd H_Cdvd;
+
+// 868 bytes
+struct H_Cdvd
+{
+    H_Cdvd* prev;             // 0x00
+    H_Cdvd* next;             // 0x04
+    u8 unkData1[0x08];
+    char path[MAX_PATH_SIZE]; // 0x10
+    u8 unkData2[0x255];
+};
+
 void H_Cdvd_BuildPathUppercase(char* srcBuf, char* dstBuf);
 
 #endif
