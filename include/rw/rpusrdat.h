a70ae83f3d7d7a7af60a710b254bc3cc4bda0f39
snaku <harischali543@gmail.com>
2026-03-27 05:07:41 +0100

    Create 'rpusrdat' files and 'RpUserDataArray' struct

---
+++ include/rw/rpusrdat.h
@@ -0,0 +1,29 @@
+#ifndef RPUSRDAT_H
+#define RPUSRDAT_H
+
+#include "rw/rwplcore.h"
+
+typedef struct RpMaterial RpMaterial; // TODO: Create in 'rpworld.h'
+
+typedef enum
+{
+    rpNAUSERDATAFORMAT = 0,
+    rpINTUSERDATA,
+    rpREALUSERDATA,
+    rpSTRINGUSERDATA,
+} RpUserDataFormat;
+
+// 16 bytes
+typedef struct RpUserDataArray
+{
+    RwChar* name;            // 0x00
+    RpUserDataFormat format; // 0x04
+    RwInt32 numElements;     // 0x08
+    void* data;              // 0x0c
+} RpUserDataArray;
+
+RwInt32 RpMaterialAddUserDataArray(RpMaterial* material, RwChar* name, RpUserDataFormat format, RwInt32 numElements);
+RwInt32 RpMaterialGetUserDataArrayCount(const RpMaterial* material);
+RpUserDataArray* RpMaterialGetUserDataArray(const RpMaterial* material, RwInt32 data);
+
+#endif
---
+++ src/rw/rpusrdat.c
@@ -0,0 +1,25 @@
+#include "rw/rpusrdat.h"
+
+// FUN_0048ee70
+RwInt32 RpMaterialAddUserDataArray(RpMaterial* material, RwChar* name, RpUserDataFormat format, RwInt32 numElements)
+{
+    // TODO
+
+    return 0;
+}
+
+// FUN_0048ee80
+RwInt32 RpMaterialGetUserDataArrayCount(const RpMaterial* material)
+{
+    // TODO
+
+    return 0;  
+}
+
+// FUN_0048eed0
+RpUserDataArray* RpMaterialGetUserDataArray(const RpMaterial* material, RwInt32 data)
+{
+    // TODO
+
+    return NULL;
+}
