d2441986ed1c0b09f6bc9a9ebf40f1a6a13e488d
snaku <harischali543@gmail.com>
2026-03-29 15:54:27 +0200

    Implement 'K_Field_Get' and 'K_Field_Init' + fix size of 'K_Field' struct

--- include/Kosaka/Field/k_field.h
+++ include/Kosaka/Field/k_field.h
@@ -5,14 +5,17 @@
 
 typedef struct Kwlntask KwlnTask;
 
-// 4424 bytes
+// 4636 bytes
 typedef struct K_Field
 {
     KwlnTask* rootTask;      // 0x00
     KwlnTask* cameraCtlTask; // 0x04
     KwlnTask* eventTask;     // 0x08
     KwlnTask* playerPadTask; // 0x0c
-    u8 unkData[0x1138];
+    u8 unkData[0x120c];
 } K_Field;
+
+void K_Field_Init();
+K_Field* K_Field_Get();
 
 #endif
--- src/Kosaka/Field/k_field.c
+++ src/Kosaka/Field/k_field.c
@@ -1,3 +1,16 @@
 #include "Kosaka/Field/k_field.h"
+#include "temporary.h"
 
 static K_Field sField; // 00869f60
+
+// FUN_001b90f0
+void K_Field_Init()
+{
+    memset(&sField, 0, sizeof(K_Field));
+}
+
+// FUN_001b9120
+K_Field* K_Field_Get()
+{
+    return &sField;
+}
