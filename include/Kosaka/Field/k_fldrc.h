e7d9e09c36073b95cdd026e50bd5f3fb62449c54
snaku <harischali543@gmail.com>
2026-03-31 06:24:26 +0200

    Implement 'K_FldRc_IsFldArchiveLoaded' and 'K_FldRc_GetFldArchiveCdvd'

--- include/Kosaka/Field/k_fldrc.h
+++ include/Kosaka/Field/k_fldrc.h
@@ -3,6 +3,10 @@
 
 #include "Utils.h"
 
+typedef struct H_Cdvd H_Cdvd;
+
 void K_FldRc_RequestFldArchive(s16 majorId, s16 minorId);
+u8 K_FldRc_IsFldArchiveLoaded();
+H_Cdvd* K_FldRc_GetFldArchiveCdvd();
 
 #endif
--- src/Kosaka/Field/k_fldrc.c
+++ src/Kosaka/Field/k_fldrc.c
@@ -2,16 +2,33 @@
 #include "h_cdvd.h"
 #include "temporary.h"
 
-static H_Cdvd* gFldArchiveCdvd; // 007ce15c. "field/pack/f%03d_%03d.pac"
+static H_Cdvd* sFldArchiveCdvd; // 007ce15c. "field/pack/f%03d_%03d.pac"
 
 // FUN_001b0870
 void K_FldRc_RequestFldArchive(s16 majorId, s16 minorId)
 {
     char buffer[64];
 
-    if (gFldArchiveCdvd == NULL)
+    if (sFldArchiveCdvd == NULL)
     {
         sprintf(buffer, "field/pack/f%03d_%03d.pac", majorId, minorId);
-        gFldArchiveCdvd = H_Cdvd_Request(buffer, H_CDVD_FILEARCHIVE);
+        sFldArchiveCdvd = H_Cdvd_Request(buffer, H_CDVD_FILEARCHIVE);
     }
+}
+
+// FUN_001b08d0
+u8 K_FldRc_IsFldArchiveLoaded()
+{
+    if (sFldArchiveCdvd == NULL)
+    {
+        return true;
+    }
+
+    return H_Cdvd_IsFileLoaded(sFldArchiveCdvd) != false;
+}
+
+// FUN_001b0910
+H_Cdvd* K_FldRc_GetFldArchiveCdvd()
+{
+    return sFldArchiveCdvd;
 }
