6b321c43f937a28c6ccb30a1d5802203e93efb51
snaku <harischali543@gmail.com>
2026-04-03 14:47:44 +0200

    Starting 'K_CollisCtl' struct

--- include/Kosaka/k_collis.h
+++ include/Kosaka/k_collis.h
@@ -6,6 +6,18 @@
 
 typedef struct Model Model;
 
+// 52 bytes. Task data for a "collision controler" task
+// Contains some info for the collisions, but also for characters movements (kinda weird ngl)
+typedef struct K_CollisCtl
+{
+    s32 unk_00;       // 0x00
+    u32 flags;        // 0x04
+    s16 resTypeId;    // 0x08. Typeid of the resrc owning the task
+    s32 unk_0c;       // 0x0c
+    Model* mdl;       // 0x10
+    u8 unkData1[0x20];
+} K_CollisCtl;
+
 
 
 #endif
