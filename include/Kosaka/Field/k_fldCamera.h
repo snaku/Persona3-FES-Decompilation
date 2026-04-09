712a1631e9e7fca645060ceaf63ca4ff6b622a5b
snaku <harischali543@gmail.com>
2026-04-09 19:07:33 +0200

    Starting 'FldCamera' struct

--- include/Kosaka/Field/k_fldCamera.h
+++ include/Kosaka/Field/k_fldCamera.h
@@ -2,15 +2,17 @@
 #define K_FLDCAMERA_H
 
 #include "Utils.h"
-#include "rw/rwplcore.h"
+#include "rw/rwcore.h"
+
+typedef struct ResrcModelParty ResrcModelParty;
 
 typedef enum
 {
     // TODO
     FLDCAMERA_TYPE_0
-} FieldCameraType;
+} FldCameraType;
 
-// 112 bytes. Content of a .CMR file
+// 112 bytes. Content of a '.CMR' file
 typedef struct CmrFile
 {
     u8 unkData1[0x08];
@@ -20,5 +22,16 @@ typedef struct CmrFile
     u16 type;          // 0x50. See enum 'FieldCameraType'
     u8 unkData2[0x1e];
 } CmrFile;
+
+// 208 bytes. Work Data for "field camera controler" task
+typedef struct FldCamera
+{
+    u8 unkData1[0x08];
+    u32 type;                     // 0x08
+    RwFrame* frame;               // 0x0c. Main frame of the field camera
+    RwFrame* frame2;              // 0x10. Parent of the parent of RwCamera (idk why)
+    u8 unkData2[0xb8];
+    ResrcModelParty* playerResrc; // 0xcc
+} FldCamera;
 
 #endif
