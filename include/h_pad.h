75f5db44a888350e3c7161ec7c8514e9a48e79c0
snaku <harischali543@gmail.com>
2026-04-14 21:24:53 +0200

    Create 'HPAD_BTN_*' and 'HPAD_CHKBTN_*' macros

--- include/h_pad.h
+++ include/h_pad.h
@@ -7,6 +7,27 @@
 #define HPAD_PORT2   HPAD_PROCESSED_PORT2 // Debug only
 #define HPAD_MAXPORT 2
 
+#define HPAD_BTN_L2       (1 << 0)  // 0x01
+#define HPAD_BTN_R2       (1 << 1)  // 0x02
+#define HPAD_BTN_L1       (1 << 2)  // 0x04
+#define HPAD_BTN_R1       (1 << 3)  // 0x08
+#define HPAD_BTN_TRIANGLE (1 << 4)  // 0x10
+#define HPAD_BTN_CIRCLE   (1 << 5)  // 0x20
+#define HPAD_BTN_CROSS    (1 << 6)  // 0x40
+#define HPAD_BTN_SQUARE   (1 << 7)  // 0x80
+#define HPAD_BTN_SELECT   (1 << 8)  // 0x100
+#define HPAD_BTN_L3       (1 << 9)  // 0x200
+#define HPAD_BTN_R3       (1 << 10) // 0x400
+#define HPAD_BTN_START    (1 << 11) // 0x800
+#define HPAD_BTN_UP       (1 << 12) // 0x1000
+#define HPAD_BTN_RIGHT    (1 << 13) // 0x2000
+#define HPAD_BTN_DOWN     (1 << 14) // 0x4000
+#define HPAD_BTN_LEFT     (1 << 15) // 0x8000
+
+#define HPAD_CHKBTN_PRESSED(port, btns)     ((gPads[(port)].btn[0].pressed & (btns)) != 0)
+#define HPAD_CHKBTN_JUSTPRESSED(port, btns) ((gPads[(port)].btn[0].justPressed & (btns)) != 0)
+#define HPAD_CHKBTN_RELEASED(port, btns)    ((gPads[(port)].btn[0].released & (btns)) != 0)
+
 typedef enum
 {
     HPAD_PROCESSED_PORT1,
