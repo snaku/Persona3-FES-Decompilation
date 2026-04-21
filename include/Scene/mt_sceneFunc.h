e756ccd8e8b43d913e038d89eff50b6f84df0cdf
snaku <harischali543@gmail.com>
2026-04-21 17:41:42 +0200

    Create 'mt_sceneFunc.c/h' files and implement 'MT_SceneFunc_CreateTasks'

---
+++ include/Scene/mt_sceneFunc.h
@@ -0,0 +1,10 @@
+#ifndef MT_SCENEFUNC_H
+#define MT_SCENEFUNC_H
+
+#include "Utils.h"
+
+typedef struct KwlnTask KwlnTask;
+
+KwlnTask* MT_SceneFunc_CreateTasks();
+
+#endif
---
+++ src/Scene/mt_sceneFunc.c
@@ -0,0 +1,47 @@
+#include "Scene/mt_sceneFunc.h"
+#include "kwln/kwlnTask.h"
+
+// FUN_003bd0b0
+void* MT_SceneFunc_UpdateSceneMngTask(KwlnTask* sceneMngTask)
+{
+    // TODO
+
+    return KWLNTASK_CONTINUE;
+}
+
+// FUN_003bcd80
+void MT_SceneFunc_DestroySceneMngTask(KwlnTask* sceneMngTask)
+{
+    // TODO
+}
+
+// FUN_003bcd80
+void* MT_SceneFunc_UpdateSceneMngDrawTask(KwlnTask* sceneMngDrawTask)
+{
+    // TODO
+    
+    return KWLNTASK_CONTINUE;
+}
+
+// FUN_003bd0b0
+KwlnTask* MT_SceneFunc_CreateTasks()
+{
+    KwlnTask* sceneMngTask;
+
+    sceneMngTask = kwlnTaskCreate(NULL,
+                                  "SceneManager Task",
+                                  110,
+                                  MT_SceneFunc_UpdateSceneMngTask,
+                                  MT_SceneFunc_DestroySceneMngTask,
+                                  NULL);
+
+
+    kwlnTaskCreate(sceneMngTask,
+                   "SceneManager Draw",
+                   2109,
+                   MT_SceneFunc_UpdateSceneMngDrawTask,
+                   NULL,
+                   NULL);
+
+    return sceneMngTask;
+}
