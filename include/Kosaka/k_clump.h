ed50e3ae2c19d08cffc96081c7374e53df1f0799
snaku <harischali543@gmail.com>
2026-04-24 19:10:44 +0200

    Implement 'K_Clump_MatUsrDataHasData'

--- include/Kosaka/k_clump.h
+++ include/Kosaka/k_clump.h
@@ -1,6 +1,11 @@
 #ifndef K_CLUMP_H
 #define K_CLUMP_H
 
-s32 K_Clump_MatGetUsrDataInt(RpMaterial* material, const char* name);
+#include "Utils.h"
+
+typedef struct RpMaterial RpMaterial;
+
+s32 K_Clump_MatUsrDataGetInt(const RpMaterial* material, const char* name);
+u32 K_Clump_MatUsrDataHasData(const RpMaterial* material, const char* name);
 
 #endif
--- src/Kosaka/k_clump.c
+++ src/Kosaka/k_clump.c
@@ -3,26 +3,52 @@
 #include "temporary.h"
 
 // FUN_001a6400
-s32 K_Clump_MatGetUsrDataInt(RpMaterial* material, const char* name)
+s32 K_Clump_MatUsrDataGetInt(const RpMaterial* material, const char* name)
 {
     s32 i;
     s32 val;
-    RpUserDataArray* userData;
+    RpUserDataArray* usrData;
     char* currName;
 
     val = 0;
     for (i = 0; i < RpMaterialGetUserDataArrayCount(material); i++)
     {
-        userData = RpMaterialGetUserDataArray(material, i);
-        currName = RpUserDataArrayGetName(userData);
+        usrData = RpMaterialGetUserDataArray(material, i);
+        currName = RpUserDataArrayGetName(usrData);
 
         if (strcmp(currName, name) == 0 &&
-           (RpUserDataArrayGetFormat(userData) == rpINTUSERDATA))
+           (RpUserDataArrayGetFormat(usrData) == rpINTUSERDATA))
         {
-            val = RpUserDataArrayGetInt(userData, 0);
+            val = RpUserDataArrayGetInt(usrData, 0);
             break;
         }
     }
 
     return val;
+}
+
+// FUN_001a64f0
+u32 K_Clump_MatUsrDataHasData(const RpMaterial* material, const char* name)
+{
+    s32 i;
+    s32 hasData;
+    RpUserDataArray* usrData;
+    char* currName;
+
+    hasData = false;
+    for (i = 0; i < RpMaterialGetUserDataArrayCount(material); i++)
+    {
+        usrData = RpMaterialGetUserDataArray(material, i);
+        RpUserDataArrayGetName(usrData); // ?
+
+        currName = RpUserDataArrayGetName(usrData);
+
+        if (strcmp(currName, name) == 0)
+        {
+            hasData = true;
+            break;
+        }
+    }
+
+    return hasData;
 }
