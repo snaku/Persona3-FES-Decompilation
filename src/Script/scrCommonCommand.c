aab33a2c78e31324a4ff24b6e93f1835cb3ac98e
snaku <harischali543@gmail.com>
2026-05-02 07:38:42 +0200

    Implement 'scrCommand_SWITCH'

--- src/Script/scrCommonCommand.c
+++ src/Script/scrCommonCommand.c
@@ -1,6 +1,7 @@
 #include "Script/scrCommonCommand.h"
 #include "Script/scrTraceCode.h"
 #include "Kosaka/k_command.h"
+#include "Kosaka/k_assert.h"
 #include "rw/rprandom.h"
 #include "datCalendar.h"
 #include "temporary.h"
@@ -8,6 +9,7 @@
 u8 scrCommand_RAND();
 u8 scrCommand_PUT();
 u8 scrCommand_PUTSTR();
+u8 scrCommand_SWITCH();
 u8 scrCommand_SQRT();
 
 // 007b92b0
@@ -48,6 +50,31 @@ u8 scrCommand_PUTSTR()
 
     param = scrGetStrPara(0);
     printf("PUTSTR -> %s\n", param);
+
+    return true;
+}
+
+// FUN_0035b110
+u8 scrCommand_SWITCH()
+{
+    s32 caseIdx;
+    s32 lblIdx;
+
+    caseIdx = scrGetIntPara(0);
+    if (caseIdx < 0)
+    {
+        return true;
+    }
+
+    K_ASSERT(caseIdx <= 6, 342);
+
+    lblIdx = scrGetIntPara(caseIdx + 1);
+    if (lblIdx < 0)
+    {
+        return true;
+    }
+
+    scrSetPC(scrGetLabelAddr(lblIdx));
 
     return true;
 }
