680f4c2c9f5e588d154f2efafd5f1dfb0ecf4f80
snaku <harischali543@gmail.com>
2026-05-22 00:09:31 +0200

    Implement 'K_FldRc_UpdateFilterTask', 'K_FldRc_DestroyFilterTask' and 'K_FldRc_CreateFilterTask'

--- include/Kosaka/Field/k_fldrc.h
+++ include/Kosaka/Field/k_fldrc.h
@@ -4,9 +4,12 @@
 #include "Utils.h"
 
 typedef struct HCdvd HCdvd;
+typedef struct KwlnTask KwlnTask;
 
 void K_FldRc_RequestFldArchive(s16 majorId, s16 minorId);
 u8 K_FldRc_IsFldArchiveLoaded();
 HCdvd* K_FldRc_GetFldArchiveCdvd();
+
+KwlnTask* K_FldRc_CreateFilterTask(KwlnTask* parent);
 
 #endif
--- src/Kosaka/Field/k_fldrc.c
+++ src/Kosaka/Field/k_fldrc.c
@@ -1,4 +1,6 @@
 #include "Kosaka/Field/k_fldrc.h"
+#include "Kosaka/Field/k_fldFilter.h"
+#include "kwln/kwlnTask.h"
 #include "h_cdvd.h"
 #include "temporary.h"
 
@@ -31,4 +33,43 @@ u8 K_FldRc_IsFldArchiveLoaded()
 HCdvd* K_FldRc_GetFldArchiveCdvd()
 {
     return sFldArchiveCdvd;
+}
+
+// FUN_001b5850
+void* K_FldRc_UpdateFilterTask(KwlnTask* fldFilterTask)
+{
+    K_FldFilter_Main();
+
+    return KWLNTASK_CONTINUE;
+}
+
+// FUN_001b5880
+void K_FldRc_DestroyFilterTask(KwlnTask* fldFilterTask)
+{
+    RwFree(fldFilterTask->workData);
+}
+
+// FUN_001b58b0
+KwlnTask* K_FldRc_CreateFilterTask(KwlnTask* parent)
+{
+    FldFilterWork* work;
+    KwlnTask* task;
+
+    work = RwCalloc(1, sizeof(FldFilterWork), rwMEMHINTDUR_GLOBAL);
+    if (work == NULL)
+    {
+        return NULL;
+    }
+
+    task = kwlnTaskCreate(parent,
+                          "field filter",
+                          4197,
+                          K_FldRc_UpdateFilterTask,
+                          K_FldRc_DestroyFilterTask,
+                          work);
+
+    work->unk_08 = 1;
+    work->unk_0c = 200;
+
+    return task;
 }
