2751e479a0ba7c6b28d7695e2de1b6ac998607ef
snaku <harischali543@gmail.com>
2026-05-27 20:15:20 +0200

    Implement 'btlFindFreeCharResId'

--- src/Battle/battle.c
+++ src/Battle/battle.c
@@ -1,8 +1,14 @@
 #include "Battle/battle.h"
 #include "kwln/kwlnTask.h"
+#include "Scene/mt_scene.h"
+#include "Scene/resrcManager.h"
 #include "temporary.h"
 
-static u64 sUID = 1; // 007cc510
+#define BTL_CHAR_RESID_BASE 0x100
+#define BTL_CHAR_RESID_MAX  0x200
+
+static u64 sUID = 1;                             // 007cc510
+static u16 sCurrCharResId = BTL_CHAR_RESID_BASE; // 007cc518
 
 Battle* gBtl; // 007ce3ec. NULL when not in a battle
 
@@ -17,6 +23,20 @@ u64 btlGetUID()
     }
 
     return sUID;
+}
+
+// FUN_0027cbd0
+u16 btlFindFreeCharResId()
+{
+    do
+    {
+        if (++sCurrCharResId >= BTL_CHAR_RESID_MAX)
+        {
+            sCurrCharResId = BTL_CHAR_RESID_BASE;
+        }
+    } while (MT_Scene_GetRes(RESRC_MAKE_TYPEID(sCurrCharResId, RESRC_TYPE_MODELCHAR)) != NULL);
+
+    return sCurrCharResId;
 }
 
 // FUN_0027d1d0
