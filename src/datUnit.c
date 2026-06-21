0368adf1857438a1e0d23ff5573e71bf924739ec
snaku <harischali543@gmail.com>
2026-06-22 01:18:20 +0200

    Implement 'datUnitCreateEc'

--- src/datUnit.c
+++ src/datUnit.c
@@ -1,4 +1,5 @@
 #include "g_data.h"
+#include "Battle/battle.h"
 #include "Kosaka/k_assert.h"
 #include "rw/rwplcore.h"
 #include "temporary.h"
@@ -23,9 +24,46 @@ DatUnitPc* datUnitCreatePc(u16 pcId)
 // FUN_002ff3e0
 DatUnitEc* datUnitCreateEc(u16 encountId)
 {
-    // TODO
+    u16 i;
+    u16 count;
+    BtlEncountTable* encount;
+    DatUnitEc* ec;
+    u16 j;
+    u16 k;
 
-    return NULL;
+    count = 0; // regswap (s1 but should be s0)
+    i = 0;
+    encount = &gEncountTbl[encountId];
+    for (; i < 5; i++)
+    {
+        if (encount->enmIds[i] != 0) // TODO: addu v0, v1, v0 instead of addu v0, v0, v1
+        {
+            count++;
+        }
+    }
+
+    ec = RwMalloc(sizeof(DatUnitEc), rwMEMHINTDUR_GLOBAL);
+    memset(ec, 0, sizeof(DatUnitEc));
+
+    ec->base.genus = UNIT_GENUS_EC;
+    ec->base.count = count;
+    ec->encountId = encountId;
+    ec->base.unit = ec->units;
+
+    j = 0;
+    for (k = 0; k < 5; k++)
+    {
+        encount = &gEncountTbl[encountId]; // TODO: addu v1, s1, v0 instead of addu v1, v0, s0
+
+        if (encount->enmIds[k] != 0) // TODO: addu v0, v1, v0 instead of addu v0, v0, v1
+        {
+            datUnitInit(&ec->base.unit[j], UNIT_GENUS_EC, encount->enmIds[k]);
+
+            j++;
+        }
+    }
+
+    return ec;
 }
 
 // FUN_002ff660
