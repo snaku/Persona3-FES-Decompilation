dd5483bf2fdbcf1e5d3ef469f109060ddb12984c
snaku <harischali543@gmail.com>
2026-06-29 00:05:00 +0200

    Implement 'btlBossCreateLoadPakPacket' and 'btlBossUpdateLoadPakPacket'

--- include/Battle/btlBoss.h
+++ include/Battle/btlBoss.h
@@ -2,9 +2,17 @@
 #define BTLBOSS_H
 
 #include "Utils.h"
+#include "Battle/btlPacket.h"
 
 typedef struct BtlCameraStateEntry BtlCameraStateEntry;
 
+typedef enum
+{
+    BTLBOSS_PACKET_LOADPAK = BTLPACKET_MAKE_ID(BTLPACKET_MODULE_BOSS, 0)
+} BtlBossPacket;
+
 const BtlCameraStateEntry* btlBossGetCameraStateEntry(u16 cameraState);
+
+BtlPacket* btlBossCreateLoadPakPacket();
 
 #endif
--- src/Battle/btlBoss.c
+++ src/Battle/btlBoss.c
@@ -1,4 +1,12 @@
 #include "Battle/btlBoss.h"
+#include "Battle/battle.h"
+#include "h_cdvd.h"
+
+// FUN_002f7dc0
+void btlBoss002f7dc0()
+{
+    // TODO
+}
 
 // FUN_002f8840
 const BtlCameraStateEntry* btlBossGetCameraStateEntry(u16 cameraState)
@@ -6,4 +14,42 @@ const BtlCameraStateEntry* btlBossGetCameraStateEntry(u16 cameraState)
     // TODO
 
     return NULL;
+}
+
+// FUN_002f9710
+void btlBossInitLoadPakPacket(void* work)
+{
+    // TODO
+}
+
+// FUN_002f9b60
+u32 btlBossUpdateLoadPakPacket(void* work)
+{
+    if (gBtl->bossPakCdvd == NULL)
+    {
+        return true;
+    }
+
+    if (H_Cdvd_IsFileLoaded(gBtl->bossPakCdvd))
+    {
+        btlBoss002f7dc0();
+
+        return true;
+    }
+
+    return false;
+}
+
+// FUN_002f9bc0
+BtlPacket* btlBossCreateLoadPakPacket()
+{
+    BtlPacket* packet;
+
+    packet = btlPacketCreate(BTLBOSS_PACKET_LOADPAK, 0);
+
+    packet->unk_47 &= ~(1 << 0);
+    packet->initFunc = btlBossInitLoadPakPacket;
+    packet->updateFunc = btlBossUpdateLoadPakPacket;
+
+    return packet;
 }
